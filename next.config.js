d7a1fa01c31eb47f0450b8876047ac54853806d3
akiba-geek <65098371+akiba-geek@users.noreply.github.com>
2024-06-05 13:00:46 +0900

    this deploys correctly

--- next.config.js
+++ next.config.js
@@ -1,3 +1,33 @@
-module.exports = {
-  output: "standalone",
-}
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true"
+})
+
+const withPWA = require("next-pwa")({
+  dest: "public"
+})
+
+module.exports = withBundleAnalyzer(
+  withPWA({
+    output: 'standalone',
+    reactStrictMode: true,
+    images: {
+      remotePatterns: [
+        {
+          protocol: "http",
+          hostname: "localhost"
+        },
+        {
+          protocol: "http",
+          hostname: "127.0.0.1"
+        },
+        {
+          protocol: "https",
+          hostname: "**"
+        }
+      ]
+    },
+    experimental: {
+      serverComponentsExternalPackages: ["sharp", "onnxruntime-node"]
+    }
+  })
+)
