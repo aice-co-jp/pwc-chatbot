3c448556500a55909fac5d4bf2cb301d17deb009
KoPiyt <108018496+KoPiyt@users.noreply.github.com>
2024-06-19 17:35:02 +0900

    チャンク化のセパレーターを追加しました

--- lib/retrieval/processing/pdf.ts
+++ lib/retrieval/processing/pdf.ts
@@ -10,6 +10,7 @@ export const processPdf = async (pdf: Blob): Promise<FileItemChunk[]> => {
   let completeText = docs.map(doc => doc.pageContent).join(" ")
 
   const splitter = new RecursiveCharacterTextSplitter({
+    separators: ["。"],
     chunkSize: CHUNK_SIZE,
     chunkOverlap: CHUNK_OVERLAP
   })
