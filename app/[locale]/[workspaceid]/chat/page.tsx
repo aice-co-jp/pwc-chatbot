89dfe5621a341868b04615475cfd83d89eeb20d2
akiba-geek <65098371+akiba-geek@users.noreply.github.com>
2024-07-07 17:53:55 +0900

    tracking down and planning changes

--- app/[locale]/[workspaceid]/chat/page.tsx
+++ app/[locale]/[workspaceid]/chat/page.tsx
@@ -6,10 +6,10 @@ import {ChatInput} from "@/components/chat/chat-input"
 //import {ChatSettings} from "@/components/chat/chat-settings"
 import {ChatUI} from "@/components/chat/chat-ui"
 //import {QuickSettings} from "@/components/chat/quick-settings"
-import {Brand} from "@/components/ui/brand"
+//import {Brand} from "@/components/ui/brand"
 import {ChatbotUIContext} from "@/context/context"
 import useHotkey from "@/lib/hooks/use-hotkey"
-import {useTheme} from "next-themes"
+//import {useTheme} from "next-themes"
 import {useContext} from "react"
 
 export default function ChatPage() {
@@ -21,8 +21,6 @@ export default function ChatPage() {
   const {chatMessages} = useContext(ChatbotUIContext)
 
   const {handleNewChat, handleFocusChatInput} = useChatHandler()
-
-  const {theme} = useTheme()
 
   return (
     <>
